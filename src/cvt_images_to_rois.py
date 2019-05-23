fa736232ca132dce664abf43f1d4afb938752205
Yongqing Liang <root@lyq.me>
2019-05-23 15:15:04 +0800

    tmp

--- src/cvt_images_to_rois.py
+++ src/cvt_images_to_rois.py
@@ -2,32 +2,25 @@ import cv2
 import numpy as np
 import os
 
-def cvt_images_to_rois(image_folder,
-                        video_path,
-                        fps = 30,
-                        fourcc = cv2.VideoWriter_fourcc(*'XVID'),
-                        stride=1):
+def cvt_images_to_rois(image_folder, out_folder):
 
     image_list = os.listdir(image_folder)
     if (len(image_list) == 0):
         exit(-1)
     image_list.sort(key = lambda x: (len(x), x))
 
-    first_image_path = os.path.join(image_folder, image_list[0])
-    height, width, channels = cv2.imread(first_image_path).shape
-    video = cv2.VideoWriter(video_path, fourcc, fps, (width, height))
+    x, y, w, h = 370, 80, 500, 500
 
-    stride = max(0, int(stride))
-    for image_idx in range(0, len(image_list), stride):
+    for image_name in image_list:
 
-        image_path = os.path.join(image_folder, image_list[image_idx])
+        image_path = os.path.join(image_folder, image_name)
         image = cv2.imread(image_path)
-        video.write(image)
+
+        out_path = os.path.join(out_folder, image_name)
+        cv2.imwrite(out_path, image[y:y+h, x:x+w,:])
         
         print("Write", image_path)
 
-    video.release()
-    
 
 if __name__ == '__main__':
     
@@ -40,4 +33,3 @@ if __name__ == '__main__':
         os.makedirs(out_folder)
 
     cvt_images_to_rois(image_folder, out_folder)
-    cvt_images_to_video(image_folder, video_path, fps, fourcc, stride)
