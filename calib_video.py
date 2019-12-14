c8c5c6a9bb97e02865fd627cf78f2ff396f01238
LyqSpace <root@lyq.me>
2019-12-14 17:17:05 -0600

    update calib

--- calib_video.py
+++ calib_video.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import configparser
+import numpy as np
 import cv2
 
 window_name = 'first frame'
@@ -16,12 +17,44 @@ def mouse_click(event, x, y, flags, param):
         pts.append((x,y))
 
     if event == cv2.EVENT_LBUTTONUP:
-        print('Point list:', pts)
+        
         cv2.circle(param, pts[-1], 5, (0, 0, 200), -1)
         cv2.imshow(window_name, param)
         
         if len(pts) == pts_n:
             loop_flag = False
+
+def calib_img(img):
+
+    global pts
+
+    cv2.namedWindow(window_name)
+    cv2.setMouseCallback(window_name, mouse_click, param = img.copy())
+
+    cv2.imshow(window_name, img)
+    while loop_flag:
+        cv2.waitKey(1)
+
+    print('Point src:', pts)
+
+    d_x = ((pts[1][0] - pts[0][0]) ** 2 + (pts[1][1] - pts[0][1]) ** 2) ** 0.5
+    d_y = ((pts[2][0] - pts[0][0]) ** 2 + (pts[2][1] - pts[0][1]) ** 2) ** 0.5
+    pts_t = [pts[0]]
+    pts_t.append((pts_t[0][0] + d_x, pts_t[0][1]))
+    pts_t.append((pts_t[0][0], pts_t[0][1] + d_y))
+    pts_t.append((pts_t[0][0] + d_x, pts_t[0][1] + d_y))
+
+    print('Point dst:', pts_t)
+
+    pts = np.float32(pts)
+    pts_t = np.float32(pts_t)
+    homo_mat, _ = cv2.findHomography(pts, pts_t)   
+    # print(homo_mat) 
+    
+    img_warped = cv2.warpPerspective(img, homo_mat, (img.shape[1], img.shape[0]))
+    cv2.imshow('warped', img_warped)
+    cv2.waitKey()
+
 
 def calib_video(video_folder):
     
@@ -29,15 +62,7 @@ def calib_video(video_folder):
     img_list.sort(key = lambda x: (len(x), x))
     img_st = cv2.imread(os.path.join(video_folder, img_list[0]))
 
-    cv2.namedWindow(window_name)
-    cv2.setMouseCallback(window_name, mouse_click, param = img_st)
-
-    cv2.imshow(window_name, img_st)
-    while loop_flag:
-        cv2.waitKey(1)
-
-    
-    
+    calib_img(img_st)
 
 
 
